959eb0d21ce6e4a5dff8cba0f9a23a4e81526943
JonathanAlles <Jonathan.Alles@evo-byte.com>
2025-01-28 23:22:54 +0100

    Feat: Add password vlaidation min 8 chars

--- readstore_basic/backend/settings/base.py
+++ readstore_basic/backend/settings/base.py
@@ -87,15 +87,18 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
+
+     {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "OPTIONS": {
+            "min_length": 8,
+        },
+    },
+
 #     {
 #         'NAME':
 # 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
 #     },
-#     {
-#         'NAME':
-# 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
 
 # 'NAME':
 # 'django.contrib.auth.password_validation.CommonPasswordValidator',
